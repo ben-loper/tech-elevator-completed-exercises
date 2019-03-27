e7bf44ab116973e7bf75be79875e1db6589638f3
Ben Loper <loperb06@gmail.com>
2019-03-27 15:05:48 -0400

    Finish Vue intro exercises

---
+++ module 4/06_Introduction_to_Vue/student-exercise/shopping-list/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <shopping-list />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import ShoppingList from './components/ShoppingList.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld,
+    ShoppingList
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+
+body {
+  background: rgb(61,201,164);
+  background: radial-gradient(circle, rgba(61,201,164,1) 50%, rgba(92,133,120,1) 100%);
+}
+</style>
